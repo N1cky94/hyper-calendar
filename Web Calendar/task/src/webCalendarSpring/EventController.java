410beb9c390b0bb5500188f3b5d08ecb89c6129b
Nick Bauters (7dbb) <nick.bauters@colruytgroup.com>
2024-12-06 14:13:12 +0100

    fix: URI for between dates was with the wrong names for the variables. Should be snake case.

--- Web Calendar/task/src/webCalendarSpring/EventController.java
+++ Web Calendar/task/src/webCalendarSpring/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Validated
@@ -20,11 +21,17 @@ public class EventController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Event>> getAllEvents() {
+    public ResponseEntity<List<Event>> getAllEvents(@RequestParam("start_date") Optional<LocalDate> startDate, @RequestParam("end_date") Optional<LocalDate> endDate) {
         var events = service.getAllEvents();
 
         if (events.isEmpty()) {
             return ResponseEntity.noContent().build();
+        }
+
+        if (startDate.isPresent() && endDate.isPresent()) {
+            events = events.stream()
+                    .filter(e -> e.getDate().isAfter(startDate.get()) && e.getDate().isBefore(endDate.get()))
+                    .toList();
         }
 
         return ResponseEntity.ok(events);
@@ -35,13 +42,6 @@ public class EventController {
         var event = service.getEventById(id);
 
         return ResponseEntity.ok(event);
-    }
-
-    @GetMapping({"{start}&{end}"})
-    public ResponseEntity<List<Event>> getEventsBetweenDates(@PathVariable LocalDate start, @PathVariable LocalDate end) {
-        var events = service.getEventsBetweenDates(new DateRange(start, end));
-
-        return ResponseEntity.ok(events);
     }
 
     @PostMapping
